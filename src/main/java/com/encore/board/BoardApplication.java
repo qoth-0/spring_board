607e5943a00d8767912519da9a793ab4bc8ad8a2
qoth-0 <bny1324@naver.com>
2024-01-30 18:33:25 +0900

    session 대신 redis 사용

--- src/main/java/com/encore/board/BoardApplication.java
+++ src/main/java/com/encore/board/BoardApplication.java
@@ -3,9 +3,11 @@ package com.encore.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableScheduling // 스케쥴링 사용시 선언
 @SpringBootApplication
+@EnableRedisHttpSession // 세션 스토리지를 redis로 사용하겠다
 public class BoardApplication {
 
 	public static void main(String[] args) {
