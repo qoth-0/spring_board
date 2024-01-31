f4bf898abe1efce98928476a44811d705db33eb0
qoth-0 <bny1324@naver.com>
2024-01-31 16:28:25 +0900

    Authentication 접근

--- src/main/java/com/encore/board/author/controller/TestController.java
+++ src/main/java/com/encore/board/author/controller/TestController.java
@@ -3,9 +3,14 @@ package com.encore.board.author.controller;
 import com.encore.board.author.service.AuthorService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @Slf4j // 로그 라이브러리(logback) 사용을 위한 어노테이션 - lombok에 포함
@@ -28,5 +33,22 @@ public class TestController {
     public String exceptionTest1(@PathVariable Long id) {
         authorService.findById(id);
         return "ok";
+    }
+
+    @GetMapping("/userinfo/test")
+    public String userInfoTest(HttpServletRequest request) {
+//        로그인 유저정보 얻는 방식
+//        방법 1 : session에 attribute를 통해 접근
+        String email1 = request.getSession().getAttribute("email").toString();
+        System.out.println("email1 " + email1);
+//        방법 2 : session에서 Authentication객체를 접근
+        SecurityContext securityContext = (SecurityContext) request.getSession().getAttribute("SPRING_SECURITY_CONTEXT");
+        String email2 = securityContext.getAuthentication().getName();
+        System.out.println("email2 " + email2);
+//        방법 3 : securityContextHolder에서 Authentication객체를 접근 - 주로 사용(1, 2번은 토큰 사용 시 세션x)
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email3 = authentication.getName();
+        System.out.println("email3 " + email3);
+        return null;
     }
 }
